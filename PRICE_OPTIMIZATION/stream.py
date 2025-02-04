0c42942c241afba88c9bebb3234f35245cfa80a6
Richard Gidi <118248129+Richard-Gidi@users.noreply.github.com>
2025-02-04 20:44:38 +0000

    Update stream.py

--- PRICE_OPTIMIZATION/stream.py
+++ PRICE_OPTIMIZATION/stream.py
@@ -23,7 +23,7 @@ import pickle
 
 
 # Load the trained model
-model = joblib.load('/content/gdrive/MyDrive/AMDARI/Jewelry Price Optimization/jewelry_price_optimizer_linear.pkl')
+model = joblib.load('models/jewelry_price_optimizer_linear.pkl')
 
 # Define model metrics (replace with your actual metrics)
 METRICS = {
